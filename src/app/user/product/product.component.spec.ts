51d6fbd27fb77de5024d9b4a71ee251aa214d9a5
rahulkant <rlkant18@gmail.com>
2019-08-13 16:31:27 -0400

    adding in the sidebar template

--- src/app/user/product/product.component.spec.ts
+++ src/app/user/product/product.component.spec.ts
@@ -26,3 +26,16 @@ describe('productComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+var figure = $('.video').hover(hoverVideo, hideVideo);
+
+function hoverVideo(e) {
+  $('video', this)
+    .get(0)
+    .play();
+}
+function hideVideo(e) {
+  $('video', this)
+    .get(0)
+    .pause();
+}
